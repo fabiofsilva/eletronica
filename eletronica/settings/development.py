ca027820cecd7ad8665e79a47a48efc0582e0a32
Fabio Ferreira da Silva <fabio_ferreiradasilva@yahoo.com.br>
2017-11-30 16:23:31 -0200

    Travis CI habilitado

--- eletronica/settings/development.py
+++ eletronica/settings/development.py
@@ -3,3 +3,9 @@ from production import *
 
 DEBUG = True
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+INSTALLED_APPS.append('django_nose')
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+NOSE_ARGS = ['--with-coverage', '--cover-package=eletronica.core']
